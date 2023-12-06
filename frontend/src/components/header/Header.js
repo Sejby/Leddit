1a558cf816d113036b067711b114ba0e9a978254
Jan Schejbal <janschejbal03@gmail.com>
2023-12-06 01:13:29 +0100

    modifying fontawesome to log and reg button in header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -6,7 +6,7 @@ import Contact from '../contact/Contact'
 import About from '../about/About'
 import Register from '../register/Register'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket,fa-right-from-bracket } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightToBracket, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 
 function Header() {
@@ -22,8 +22,8 @@ function Header() {
 
           <input type="text" placeholder="Uživatelské jméno" name="uzivatelskejmeno" id="username_input"/>
           <input type="password" placeholder="Heslo" name="heslo" id="pwd_input"/>
-          <button type='submit' className='btn btn-success btn-sm'><FontAwesomeIcon icon={faRightFromBracket}/>Přihlásit</button>
-          <Link to="/registrace"><button type='submit' className='btn btn-danger btn-sm'>Registrovat<FontAwesomeIcon icon={faRightFromBracket}/></button><i class="fa-solid fa-right-from-bracket"></i></Link>
+          <button type='submit' className='btn btn-success btn-sm'><FontAwesomeIcon icon={faArrowRightToBracket}/> Přihlásit</button>
+          <Link to="/registrace"><button type='submit' className='btn btn-danger btn-sm'>Registrovat <FontAwesomeIcon icon={faArrowRightFromBracket}/></button></Link>
         </div>
     </header>
 
